ffdbcd13dceab1c01148d928bc8ecaae65b00e05
albin29 <albin.duraku00@gmail.com>
2024-01-20 14:51:37 +0100

    initial commit

---
+++ UDPClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPAddress broadcast = IPAddress.Parse("192.168.1.157");
+        Console.WriteLine("Enter the message to be sent");
+        var message = Console.ReadLine();
+        while (message != String.Empty)
+        {
+            byte[] sendbuf = Encoding.UTF8.GetBytes(message);
+            IPEndPoint ep = new IPEndPoint(broadcast, 27015);
+
+            s.SendTo(sendbuf, ep);
+            Console.WriteLine("Message sent");
+            message = Console.ReadLine();
+        }
+        Console.ReadLine();
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+public class UDP
+{
+    private const int listenPort = 27015;
+
+    private static void StartListener()
+    {
+        UdpClient listener = new UdpClient(listenPort);
+        IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting for msg");
+                byte[] bytes = listener.Receive(ref groupEP);
+                Console.WriteLine($"Received msg from {groupEP} : ");
+                Console.WriteLine($"{Encoding.UTF8.GetString(bytes, 0, bytes.Length)}");
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            listener.Close();
+        }
+    }
+
+    public static void Main()
+    {
+        StartListener();
+    }
+
+
+}
